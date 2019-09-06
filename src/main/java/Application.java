dd612dcef384e360908cd611f8c26f4daa241de0
Jack Mead <jack.mead@softwire.com>
2019-09-06 09:09:41 +0100

    Made entry point run some games between bots

---
+++ src/main/java/Application.java
@@ -0,0 +1,47 @@
+import bots.RandomBot;
+import bots.RandomDynamiteBot;
+import bots.RockBot;
+import bots.YourBot;
+import com.softwire.dynamite.bot.Bot;
+
+public class Application {
+    private GameRunner gameRunner = new GameRunner();
+
+    public static void main(String[] args) {
+        new Application().Run();
+    }
+
+    public void Run() {
+
+        RunGameBetween(new YourBot(), new RockBot());
+
+        RunGameBetween(new YourBot(), new RandomBot());
+
+        RunGameBetween(new YourBot(), new RandomDynamiteBot());
+    }
+
+    private void RunGameBetween(Bot botOne, Bot botTwo){
+        printGameResult(gameRunner.RunGame(botOne, botTwo));
+        System.out.println();
+    }
+
+    private void printGameResult(GameResult gameResult) {
+        String resultString = String.format("%s: %d vs %s: %d",
+                gameResult.getPlayerOneName(),
+                gameResult.getPlayerOneScore(),
+                gameResult.getPlayerTwoName(),
+                gameResult.getPlayerTwoScore());
+        System.out.println(resultString);
+
+        if (gameResult.getPlayerOneScore() == gameResult.getPlayerTwoScore()) {
+            System.out.println("*** Draw ***");
+            return;
+        }
+
+        String winnerName = gameResult.getPlayerOneScore() > gameResult.getPlayerTwoScore() ?
+                gameResult.getPlayerOneName() :
+                gameResult.getPlayerTwoName();
+        System.out.println(String.format("*** Winner: %s ***", winnerName));
+
+    }
+}
